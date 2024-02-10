0d644f9fd782237c3ef3a51a45ad4db77d8e68ab
parkgeonwoong <hk77kh@gmail.com>
2024-02-10 18:00:08 +0900

    feat : methods 객체와 vue 앱 내에서 데이터 작업

--- 2.dom/2-1.instance/app.js
+++ 2.dom/2-1.instance/app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "뷰를 배우기",
+      courseGoalA: "Vue를 배우기",
+      courseGoalB: "Vue를 마스터하기",
       vueLink: "https://ko.vuejs.org/",
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
   },
 });
 
