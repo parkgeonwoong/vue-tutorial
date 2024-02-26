bc8906b54cf02b787f481864d243b810e5921fa1
parkgeonwoong <hk77kh@gmail.com>
2024-02-26 17:23:16 +0900

    feat : 회복 기능 추가하기

--- 0.project/1.monsterSlayer/app.js
+++ 0.project/1.monsterSlayer/app.js
@@ -20,6 +20,9 @@ const app = Vue.createApp({
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
     },
+    stopPlayerHeal() {
+      return this.playerHealth === 100;
+    },
   },
   methods: {
     attackMonster() {
@@ -36,6 +39,16 @@ const app = Vue.createApp({
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
       this.attackPlayer();
     },
   },
