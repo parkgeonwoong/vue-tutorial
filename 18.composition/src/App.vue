6ed3c7e706330d0033e00467ded3b1afa0dc65a3
parkgeonwoong <hk77kh@gmail.com>
2024-09-25 10:38:50 +0900

    feat : [composition] 반응형 객체 구축

---
+++ 18.composition/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="container">
+    <h2>{{ user.userName }}</h2>
+    <h2>{{ user.age }}</h2>
+
+    <h2>{{ user2.userName }}</h2>
+    <h2>{{ user2.age }}</h2>
+  </section>
+</template>
+
+<script setup>
+import { reactive, ref, isRef, isReactive, toRefs } from 'vue';
+
+const user = ref({
+  userName: 'ref hello',
+  age: 30
+});
+const user2 = reactive({
+  userName: 'reactive hello',
+  age: 31
+});
+
+console.log(isRef(user.value));
+console.log(isReactive(user2.age));
+console.log(toRefs(user2));
+
+setTimeout(() => {
+  user.value.userName = 'ref world';
+  user.value.age = 40;
+
+  user2.userName = 'reactive world';
+  user2.age = 41;
+}, 2000);
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
