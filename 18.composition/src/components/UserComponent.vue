d7b3e0e6664a6fb06cc71a143e239f3846299da1
parkgeonwoong <hk77kh@gmail.com>
2024-09-26 11:49:26 +0900

    feat : [composition] computed & 양방향 바인딩

--- 18.composition/src/components/UserComponent.vue
+++ 18.composition/src/components/UserComponent.vue
@@ -7,19 +7,27 @@
     <h2>{{ user2.age }}</h2>
 
     <button @click="updateUser">Change age</button>
+
+    <hr />
+
+    <div>
+      <input type="text" placeholder="first name" v-model="firstName" />
+      <input type="text" placeholder="last name" v-model="lastName" />
+    </div>
+    <h2>{{ fullName }}</h2>
   </section>
 </template>
 
 <script setup>
-import { reactive, ref, isRef, isReactive, toRefs } from 'vue';
+import { reactive, ref, isRef, isReactive, toRefs, computed } from 'vue';
 
-const user = ref({
-  userName: 'ref hello',
-  age: 30
-});
-const user2 = reactive({
-  userName: 'reactive hello',
-  age: 31
+const user = ref({ userName: 'ref hello', age: 30 });
+const user2 = reactive({ userName: 'reactive hello', age: 31 });
+const firstName = ref('');
+const lastName = ref('');
+
+const fullName = computed(() => {
+  return firstName.value + ' ' + lastName.value;
 });
 
 function updateUser() {
@@ -30,13 +38,13 @@ console.log(isRef(user.value));
 console.log(isReactive(user2.age));
 console.log(toRefs(user2));
 
-setTimeout(() => {
-  user.value.userName = 'ref world';
-  user.value.age = 40;
+// setTimeout(() => {
+//   user.value.userName = 'ref world';
+//   user.value.age = 40;
 
-  user2.userName = 'reactive world';
-  user2.age = 41;
-}, 2000);
+//   user2.userName = 'reactive world';
+//   user2.age = 41;
+// }, 2000);
 </script>
 
 <style>
