77cbd95f444d43213cb48b332844fcb62000b7ad
parkgeonwoong <hk77kh@gmail.com>
2024-09-27 11:21:59 +0900

    feat : 생명주기 사이클 및 연습

---
+++ 18.composition/18-1.study/src/components/UserData.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h2>{{ fullName }}</h2>
+    <h2>{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated
+} from 'vue';
+
+export default {
+  props: ['firstName', 'lastName'],
+  setup(props, contexts) {
+    const fullName = computed(() => {
+      return props.firstName + ' ' + props.lastName;
+    });
+    console.log('contexts: ', contexts);
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount: ');
+    });
+    onMounted(() => {
+      console.log('onMounted: ');
+    });
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate: ');
+    });
+    onUpdated(() => {
+      console.log('onUpdated: ');
+    });
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount: ');
+    });
+    onUnmounted(() => {
+      console.log('onUnmounted: ');
+    });
+
+    const age = inject('userAge');
+
+    return {
+      fullName,
+      age
+    };
+  }
+};
+</script>
