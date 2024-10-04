581054f2e8b36fc7380d8f3468390eadb41f2b96
parkgeonwoong <hk77kh@gmail.com>
2024-10-04 11:48:15 +0900

    feat : Optional 컴포넌트화

--- 5.component/src/App.vue
+++ 5.component/src/App.vue
@@ -1,31 +1,42 @@
-<script setup>
+<!-- Options API -->
+<script>
+import FriendContact from './components/FriendContact.vue';
 
+export default {
+  components: {
+    FriendContact
+  },
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '01234 5678 991',
+          email: 'manuel@localhost'
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '09876 543 221',
+          email: 'julie@localhost'
+        }
+      ]
+    };
+  }
+};
 </script>
+
+<!-- Composition API -->
+<!-- <script setup></script> -->
 
 <template>
   <header>
-      <h1>FriendList</h1>
-    </header>
-    <section id="app">
-      <ul>
-        <li>
-          <h2>Manuel Lorenz</h2>
-          <button>Show Details</button>
-          <ul>
-            <li><strong>Phone:</strong> 01234 5678 991</li>
-            <li><strong>Email:</strong> manuel@localhost.com</li>
-          </ul>
-        </li>
-        <li>
-          <h2>Julie Jones</h2>
-          <button>Show Details</button>
-          <ul>
-            <li><strong>Phone:</strong> 09876 543 221</li>
-            <li><strong>Email:</strong> julie@localhost.com</li>
-          </ul>
-        </li>
-      </ul>
-    </section>
+    <h1>FriendList</h1>
+  </header>
+  <section>
+    <ul>
+      <FriendContact v-for="friend in friends" :key="friend.id" :friend="friend" />
+    </ul>
+  </section>
 </template>
-
-
---
+++ 5.component/src/components/FriendContact.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: ['friend'],
+  data() {
+    return {
+      detailAreaVisible: false
+    };
+  },
+  methods: {
+    toggleDetailArea() {
+      this.detailAreaVisible = !this.detailAreaVisible;
+    }
+  }
+};
+</script>
+
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetailArea">{{ detailAreaVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailAreaVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
