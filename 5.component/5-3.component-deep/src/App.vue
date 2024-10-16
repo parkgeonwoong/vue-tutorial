29b0ba94d4e5ad48120e9f1421600a8f06cadac8
parkgeonwoong <hk77kh@gmail.com>
2024-10-16 14:03:02 +0900

    init : component-deep init

---
+++ 5.component/5-3.component-deep/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
+const activeUser = ref({
+  name: 'Maximilian Schwarzmüller',
+  description: 'Site owner and admin',
+  role: 'admin',
+});
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ 5.component/5-3.component-deep/src/components/BaseBadge.vue
@@ -0,0 +1,35 @@
+<template>
+  <span class="badge" :class="classes">{{ caption }}</span>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps(['type', 'caption']);
+const classes = computed(() => {
+  return {
+    'badge--admin': props.type === 'admin',
+    'badge--author': props.type === 'author',
+  };
+});
+</script>
+
+<style>
+.badge {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  border-radius: 30px;
+  background-color: #ccc;
+  color: #2e2e2e;
+}
+
+.badge--admin {
+  background-color: #810036;
+  color: white;
+}
+
+.badge--author {
+  background-color: #002c8a;
+  color: white;
+}
+</style>
