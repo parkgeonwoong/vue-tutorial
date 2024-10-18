08e35a136bb81225ba52960842320dae4d6153cd
parkgeonwoong <hk77kh@gmail.com>
2024-10-18 11:32:44 +0900

    feat : 동적 컴포넌트 적용

--- 5.component/5-3.component-deep/src/App.vue
+++ 5.component/5-3.component-deep/src/App.vue
@@ -13,21 +13,33 @@
     </CourseGoal>
 
     <hr />
+    <button @click="setActiveComponent(ActiveGoals)">Active Goals</button>
+    <button @click="setActiveComponent(ManageGoals)">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponent" />
+    </keep-alive>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, shallowRef } from 'vue';
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
 import CourseGoal from './components/CourseGoal.vue';
+import ManageGoals from './components/ManageGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
 
 const activeUser = ref({
   name: 'Maximilian Schwarzmüller',
   description: 'Site owner and admin',
   role: 'admin',
 });
+const selectedComponent = shallowRef(ActiveGoals);
+
+const setActiveComponent = component => {
+  selectedComponent.value = component;
+};
 </script>
 
 <style>
